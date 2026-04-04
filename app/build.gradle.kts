957dadd7331ade962e0f47ba02165332491556d2
Sergei Smirnov <sergei.smirnov@cleverpumpkin.ru>
2026-04-04 19:07:24 +0300

    added koin and ktor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,21 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     debugImplementation(libs.androidx.compose.ui.tooling)
+
+    //Koin
+    implementation(project.dependencies.platform(libs.koin.bom))
+    implementation(libs.koin.core)
+
+    // Ktor
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.client.json)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.serialization)
+    implementation(libs.ktor.serialization.json)
+    implementation(libs.ktor.client.logging)
+
+    // Ktorfit
+    implementation(libs.ktorfit.lib)
+
 }
